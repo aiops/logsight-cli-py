d4e9cd8222d302df011bcd0aa5efeb700b66b1a4
Jorge Cardoso <jorge.cardoso.pt@gmail.com>
2022-06-26 13:23:44 +0200

    chg: updated config command to generate json

--- logsight_cli/logsight_cli.py
+++ logsight_cli/logsight_cli.py
@@ -2,8 +2,12 @@
 
 import sys
 import os
+import json
 from configparser import ConfigParser
 from pathlib import Path
+import pkg_resources
+from packaging import version
+import importlib.metadata
 
 import json as js
 import click
@@ -12,11 +16,17 @@ from prettytable import PrettyTable
 import logsight.config
 from logsight.authentication import LogsightAuthentication
 
-from logsight_cli.application import application
 from logsight_cli.log import log
 from logsight_cli.compare import compare
 from logsight_cli.incident import incident
 from logsight_cli.utils import utils
+
+
+LOGSIGHT_VERSION_MIN = '0.2.6'
+if version.parse(importlib.metadata.version('logsight-sdk-py')) < \
+        version.parse(LOGSIGHT_VERSION_MIN):
+    raise EnvironmentError(
+        f'logsight-sdk-py version too low, required >= {LOGSIGHT_VERSION_MIN}')
 
 
 PYTHON_VERSION_MIN = (3, 8, 0)
@@ -25,7 +35,7 @@ if not sys.version_info >= PYTHON_VERSION_MIN:
         f'Python version too low, required >= {".".join(str(n) for n in PYTHON_VERSION_MIN)}')
 
 
-LOGSIGHT_OPTIONS = ["HOST_API", "EMAIL", "PASSWORD", "APP_ID", "DEBUG", "JSON"]
+LOGSIGHT_OPTIONS = ['HOST_API', 'EMAIL', 'PASSWORD', 'DEBUG', 'JSON']
 CONFIG = dict.fromkeys(LOGSIGHT_OPTIONS, (None, None))
 
 CONFIG_FILE = os.path.join(str(Path.home()), ".logsight")
@@ -47,19 +57,19 @@ CONFIG.update(
     }
 )
 
-VERSION = '0.0.48'
+
+VERSION = pkg_resources.require('logsight-cli-py')[0].version
 
 
 @click.group(help="CLI tool to manage logsight.ai artifacts")
-@click.version_option(VERSION, prog_name="Logsight CLI")
+@click.version_option(VERSION, prog_name="Logsight.ai CLI")
 @click.pass_context
 @click.option("--debug/--no-debug")
+@click.option("--json", help="Output as JSON.", is_flag=True)
 @click.option("--host_api", help="Logsight host API.")
 @click.option("--email", help="User e-mail.")
 @click.option("--password", help="User password.")
-@click.option("--json", help="Output as JSON.", is_flag=True)
-@click.option("--app_id", help="Default app ID.")
-def cli(ctx, debug, json, host_api, email, password, app_id):
+def cli(ctx, debug, json, host_api, email, password):
 
     for k in LOGSIGHT_OPTIONS:
         if locals().get(k.lower()):
@@ -68,7 +78,7 @@ def cli(ctx, debug, json, host_api, email, password, app_id):
     for k in LOGSIGHT_OPTIONS:
         ctx.obj[k] = CONFIG[k][0]
         if k in ['DEBUG', 'JSON'] and ctx.obj[k] is not None:
-            ctx.obj[k] = js.loads(ctx.obj[k].lower())
+            ctx.obj[k] = js.loads(str(ctx.obj[k]).lower())
 
     if ctx.obj['HOST_API']:
         logsight.config.set_host(ctx.obj['HOST_API'])
@@ -83,20 +93,27 @@ def cli(ctx, debug, json, host_api, email, password, app_id):
 
 
 @cli.command()
-def config():
+@click.pass_context
+def config(ctx):
     """
     Show configuration
     """
-    echo_config()
+    echo_config(ctx)
     sys.exit(0)
 
 
-def echo_config():
-    table = PrettyTable(["OPTION", "VALUE", "SOURCE"])
-    table.align = "l"
-    for i in LOGSIGHT_OPTIONS:
-        table.add_row([i, CONFIG[i][0], CONFIG[i][1]])
-    click.echo(table)
+def echo_config(ctx):
+    keys = ['option', 'value', 'source']
+    config = [{keys[0]: i, keys[1]: CONFIG[i][0], keys[2]: CONFIG[i][1]} for i in LOGSIGHT_OPTIONS]
+    if ctx.obj['JSON']:
+        s = json.dumps(config, sort_keys=True, indent=4)
+        click.echo(s)
+    else:
+        table = PrettyTable(keys)
+        table.align = "l"
+        for opt in config:
+            table.add_row([opt.get(k, '') for k in keys])
+        click.echo(table)
 
 
 cli.add_command(log.log)
--- tests/test_config.py
+++ tests/test_config.py
@@ -2,8 +2,7 @@ from click.testing import CliRunner
 
 from logsight_cli.logsight_cli import cli
 
-
-AUTH = ['--email', 'jorge.cardoso.pt@gmail.com', '--password', 'xymneq-jasqA9-faxtyz']
+AUTH = ['--json', '--email', 'logsight.testing.001@gmail.com', '--password', 'hibhiv-5hurce-zovnyG']
 
 
 def test_config():
@@ -11,6 +10,14 @@ def test_config():
     result = runner.invoke(cli, AUTH + ['config'], obj={})
     assert result.exit_code == 0
     assert 'EMAIL' in result.output
+    assert AUTH[2] in result.output
+    assert AUTH[4] in result.output
+
+
+def test_compare_ls():
+    runner = CliRunner()
+    result = runner.invoke(cli, AUTH + ['compare', 'ls'], obj={})
+    assert result.exit_code == 0
 
 
 if __name__ == '__main__':
